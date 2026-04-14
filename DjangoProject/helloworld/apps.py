63f984344d110dfff94a4ae731b0b9058ff0d916
Nullyfc <71546280+Nullyfc@users.noreply.github.com>
2026-04-14 19:08:25 +0800

    Delete DjangoProject directory

--- DjangoProject/helloworld/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class HelloworldConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'helloworld'
-    verbose_name = "网站图书管理"
